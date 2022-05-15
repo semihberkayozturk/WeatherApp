e26c78dda215cfd29c5dc933841752b6677733af
Semih Berkay Öztürk <90466553+semihberkayozturk@users.noreply.github.com>
2022-05-15 10:10:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
 
     const query = req.body.cityName;
-    const apiKey = "69f04e4613056b159c2761a9d9e664d2";
+    const apiKey = "your api key";
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
     https.get(url, function(response) {
